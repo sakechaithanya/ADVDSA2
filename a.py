808ef3a8275468415bedc78face52d4c2731810e
SAKE CHAITANYA <61873017+sakechaithanya@users.noreply.github.com>
2023-05-08 13:57:41 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,66 +1,198 @@
-# admin_panel.py
-from s import * 
-import random 
-class AdminPanel:
+# Implement Binary Search
 
-    def __init__(self):
-        self.total_module_dict = {}
-        self.student_details = {}
+def binarySearch(array, x, low, high):
 
-    def admin_login(self, username, password):
-        if username == "sake" and password == "chay":
-            return True
-        return False
+    # Repeat until the pointers low and high meet each other
+    while low <= high:
 
-    def add_module(self, module_name, duration):
-        topic_list = []
-        
-        
-        topic_size = int(input("Enter the no. of topics for this module:"))
-        for i in range(topic_size):
-            topic = input("enter topic name:")
-            topic_list.append(topic)
+        mid = low + (high - low)//2
+
+        if array[mid] == x:
+            return mid
+
+        elif array[mid] < x:
+            low = mid + 1
+
+        else:
+            high = mid - 1
+
+    return -1
 
 
-        module_data = {
-            "module_name": module_name,
-            "duration": duration,
-            "topics": topic_list
-        }
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
 
-        self.total_module_dict[module_name]= module_data
-        print("printitng from inside total module dict function", self.total_module_dict)
-        return self.total_module_dict
+result = binarySearch(array, x, 0, len(array)-1)
 
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
 
-    def add_student(self):
-        
-        student_size = int(input("enter the number of students to be added:"))
+# Implement Merge Sort
+def merge(arr, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+ 
+    # create temp arrays
+    L = [0] * (n1)
+    R = [0] * (n2)
+ 
+    # Copy data to temp arrays L[] and R[]
+    for i in range(0, n1):
+        L[i] = arr[l + i]
+ 
+    for j in range(0, n2):
+        R[j] = arr[m + 1 + j]
+ 
+    # Merge the temp arrays back into arr[l..r]
+    i = 0     # Initial index of first subarray
+    j = 0     # Initial index of second subarray
+    k = l     # Initial index of merged subarray
+ 
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    # Copy the remaining elements of L[], if there
+    # are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+ 
+    # Copy the remaining elements of R[], if there
+    # are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+ 
+# l is for left index and r is right index of the
+# sub-array of arr to be sorted
+ 
+ 
+def mergeSort(arr, l, r):
+    if l < r:
+ 
+        # Same as (l+r)//2, but avoids overflow for
+        # large l and h
+        m = l+(r-l)//2
+ 
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+ 
+ 
+# Driver code to test above
+arr = [12, 11, 13, 5, 6, 7]
+n = len(arr)
+print("Given array is")
+for i in range(n):
+    print("%d" % arr[i],end=" ")
+ 
+mergeSort(arr, 0, n-1)
+print("\n\nSorted array is")
+for i in range(n):
+    print("%d" % arr[i],end=" ")
 
-        for i in range(1, student_size+1):
-            print("Enter details of students below as asked:")
-            name = input("enter name:")
-            gender = input("enter gender:")
-            age = input("enetr age:")
-            mobile_no = input("enetr mobile no.")
-            email = input("enter email")
-            password = input("enter student pwd")
-            student = Student(i, name, gender, age, mobile_no, email, password)
+# Implement Quick Sort
+def partition(array, low, high):
+ 
+    # Choose the rightmost element as pivot
+    pivot = array[high]
+ 
+    # Pointer for greater element
+    i = low - 1
+ 
+    # Traverse through all elements
+    # compare each element with pivot
+    for j in range(low, high):
+        if array[j] <= pivot:
+            # If element smaller than pivot is found
+            # swap it with the greater element pointed by i
+            i = i + 1
+ 
+            # Swapping element at i with element at j
+            (array[i], array[j]) = (array[j], array[i])
+ 
+    # Swap the pivot element with
+    # e greater element specified by i
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+ 
+    # Return the position from where partition is done
+    return i + 1
+ 
+# Function to perform quicksort
+ 
+ 
+def quick_sort(array, low, high):
+    if low < high:
+ 
+        # Find pivot element such that
+        # element smaller than pivot are on the left
+        # element greater than pivot are on the right
+        pi = partition(array, low, high)
+ 
+        # Recursive call on the left of pivot
+        quick_sort(array, low, pi - 1)
+ 
+        # Recursive call on the right of pivot
+        quick_sort(array, pi + 1, high)
+ 
+ 
+# Driver code
+array = [10, 7, 8, 9, 1, 5]
+quick_sort(array, 0, len(array) - 1)
+ 
+print(f'Sorted array: {array}')
+ 
+# Implement Insertion Sort
 
-            student_dict = {'ID': i, 'name': name, 'gender': gender, 'age': age, 
-                            'mobile_no': mobile_no, 'email': email, 'pwd': password}
-            
-            self.student_details[i] = student_dict
-        # print("printing student details:", self.student_details)
+def insertionSort(arr):
+ 
+    # Traverse through 1 to len(arr)
+    for i in range(1, len(arr)):
+ 
+        key = arr[i]
+ 
+        # Move elements of arr[0..i-1], that are
+        # greater than key, to one position ahead
+        # of their current position
+        j = i-1
+        while j >= 0 and key < arr[j] :
+                arr[j + 1] = arr[j]
+                j -= 1
+        arr[j + 1] = key
+ 
+ 
+# Driver code to test above
+arr = [12, 11, 13, 5, 6]
+insertionSort(arr)
+for i in range(len(arr)):
+    print ("% d" % arr[i])
 
-        return self.student_details
-            
-
-
-
-
-
-
-ap = AdminPanel()
-x= ap.admin_login("abc", "abc")
-print(x)
+# # initializing list
+test_list = ['gfg', 'is', 'best']
+ 
+# initializing Dictionary
+test_dict = {'gfg' : 56, 'is' : 12, 'best' : 76}
+ 
+# printing original list
+print("The original list is : " + str(test_list))
+ 
+# printing original dictionary
+print("The original dictionary is : " + str(test_dict))
+ 
+# Sort List by Dictionary values
+# Using sorted() + key + lambda
+res = sorted(test_list, key = lambda ele: test_dict[ele])
+ 
+# printing result
+print("The list after sorting : " + str(res))
